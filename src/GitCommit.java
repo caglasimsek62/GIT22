45cb958aa21efb5c1cf1ba851c870e2c31bff622
caglasimsek62 <caglasimsek@Caglas-Laptop.lan>
2024-07-15 20:34:57 -0400

    second commit

---
+++ src/GitCommit.java
@@ -0,0 +1,7 @@
+public class GitCommit {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("git add ");
+        System.out.println("git commit");
+    }
+}
