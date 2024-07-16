9016b19668d41b2b8a5c9b248e9ba4ef8935695f
caglasimsek62 <caglasimsek@Caglas-Laptop.lan>
2024-07-15 21:11:07 -0400

    second push

--- src/GitClass.java
+++ src/GitClass.java
@@ -2,7 +2,7 @@ public class GitClass {
     public static void main(String[] args) {
         System.out.println("Hello GIT");
         System.out.println("Git add");
-
+        System.out.println("git push");
 
 
 
